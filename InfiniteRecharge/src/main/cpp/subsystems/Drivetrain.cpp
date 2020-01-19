d6bab8e81380786f103de402d5624fa02ac0c5f5
jcrosbyME <john-crosby@idexx.com>
2020-01-19 12:50:27 -0500

    Removed and added Comment in DriveTrain

--- InfiniteRecharge/src/main/cpp/subsystems/Drivetrain.cpp
+++ InfiniteRecharge/src/main/cpp/subsystems/Drivetrain.cpp
@@ -17,7 +17,7 @@ Drivetrain::Drivetrain() {
     rightFollowerSpark1.reset(new rev::CANSparkMax(Constants::Drivetrain::rightFollower1, rev::CANSparkMax::MotorType::kBrushless));
     rightFollowerSpark2.reset(new rev::CANSparkMax(Constants::Drivetrain::rightFollower2, rev::CANSparkMax::MotorType::kBrushless));
 
-////TODO: Why are these prototyped here in the Constructor, is this new syntax requirement??? They are already prototyped in the Header File...///
+////Execute Methods to set up Motor Colntrollers (Followers, Ramping Rates, and Inverted Motors)
     SetFollowers();
     SetRamp();
     InvertFollowers();
