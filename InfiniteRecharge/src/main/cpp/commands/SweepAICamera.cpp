d471c74119454d7528958153691495be9246295f
Hunter-Pellerin <hpellerin2003@gmail.com>
2020-02-15 21:58:58 -0500

    complete asynchronous function call may fix servos

--- InfiniteRecharge/src/main/cpp/commands/SweepAICamera.cpp
+++ InfiniteRecharge/src/main/cpp/commands/SweepAICamera.cpp
@@ -8,6 +8,7 @@
 #include "commands/SweepAICamera.h"
 #include <iostream>
 #include <functional>
+#include <future>
 
 SweepAICamera::SweepAICamera() {
   AddRequirements(RobotContainer::cameraMount.get());
@@ -18,8 +19,9 @@ void SweepAICamera::Initialize() {}
 
 // Called repeatedly when this Command is scheduled to run
 void SweepAICamera::Execute() {
-  std::cout << "running sweep command\n"; 
-  RobotContainer::cameraMount->SweepForPowercells();
+  std::cout << "running sweep command\n";
+  std::future<bool> awaitSweep = std::async(RobotContainer::cameraMount->IntervaledExecution, RobotContainer::cameraMount->SweepForPowercells, 10);
+  awaitSweep.get();
   //Query AI for Powercell in frame
   /*
   if(!TargFound)
