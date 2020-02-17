253cba40cd578a9c658409bba66c5a5e728ab0a1
ADever <55519742+ADever@users.noreply.github.com>
2020-02-17 16:04:05 -0500

    returned distance and strength in inches

--- InfiniteRecharge/python/tfmini.py
+++ InfiniteRecharge/python/tfmini.py
@@ -22,10 +22,12 @@ def getTFminiData():
             ser.reset_input_buffer()
             int(recv[0])
             if recv[0] == 0x59 and recv[1] == 0x59:
-                distance = recv[2] + recv[3] * 256
+                cmdistance = recv[2] + recv[3] * 256
+                distance = cmdistance/2.54
                 strength = recv[4] + recv[5] * 256
                 print('(', distance, ',', strength, ')')
                 ser.reset_input_buffer()
+                return (distance, strength)
 #python3                  
 if __name__ == '__main__':
     getTFminiData()
@@ -34,4 +36,4 @@ if __name__ == '__main__':
             ser.open()
             getTFminiData()
     except:
-        print("error")
+        print("error, no serial port available")
