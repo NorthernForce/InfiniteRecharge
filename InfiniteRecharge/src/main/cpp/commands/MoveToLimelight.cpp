5e8ca1799e2824c71c4e8026fc3dc479e4b666b6
ADever <aiden.dever@gorhamschools.org>
2020-03-05 17:55:43 -0500

    Limelight stuff, pushing for test

--- InfiniteRecharge/src/main/cpp/commands/MoveToLimelight.cpp
+++ InfiniteRecharge/src/main/cpp/commands/MoveToLimelight.cpp
@@ -23,6 +23,8 @@ void MoveToLimelight::Initialize() {
 void MoveToLimelight::Execute() {
   limeLightOffset = RobotContainer::limelight->GetXOffset();
   isTargetThere = RobotContainer::limelight->IsTargetThere();
+  std::cout << "is target there: " << isTargetThere << std::endl;
+  std::cout << "limelight offset: " << limeLightOffset << std::endl;
   if (isTargetThere == true) {
     TurnToAngle(limeLightOffset);
   }
