08fce5245eda25a3b702c51fc3270b3d9385d585
Student <trevin.macomber@gmail.com>
2020-06-16 11:11:32 -0400

    Removed Intake

--- InfiniteRecharge/src/main/cpp/commands/IntakePowerCell.cpp
+++ InfiniteRecharge/src/main/cpp/commands/IntakePowerCell.cpp
@@ -31,97 +31,10 @@ void IntakePowerCell::Initialize() {
 ////TODO: NEED to check if Pos. 5 is empty before starting Intake!!
 //****************************************************************************
 // Called repeatedly when this Command is scheduled to run
-void IntakePowerCell::Execute() {
-  
-  std::cout << "Empty Position is " << emptyPosition <<  "\n";
-  std::cout << "Empty Position Triggered: " << emptyPositionTriggered << "\n";
-  std::cout << "Bad intake is " << badIntake <<  "\n";
-  //std::cout << "Pos 5 Triggered: " << fiveReached << "\n";
-
-  if (RobotContainer::intake->GetInventory(emptyPosition) == Intake::StorageState::PRESENT) {
-    std::cout << "Empty Position " << emptyPosition <<  " full\n";
-  }
-  else {
-    std::cout << "Empty Position " << emptyPosition <<  " empty\n";
-  }
-
-  if (RobotContainer::intake->GetInventory(5) == Intake::StorageState::PRESENT) {
-    RobotContainer::intake->StopConveyor();
-  }
-
-/*
-  //stops conveyor when power cell has cleared pos 0
-  if (RobotContainer::intake->GetInventory(5) == Intake::StorageState::PRESENT && conveyorBackwardsCounter <= 2 && fiveReached == false) {
-
-    //controls how long conveyor goes backward for
-    if (conveyorBackwardsCounter >= backwardCountLimit) {
-      RobotContainer::intake->StopConveyor();
-      //conveyorBackwardsCounter = 0;
-    }
-
-    //move conveyor backward to try and brake faster 
-    if (conveyorBackwardsCounter == 0) {
-      RobotContainer::intake->ConveyorSetSpeed(conveyorBackwardSpeed);
-    }
-    conveyorBackwardsCounter++;
-  } 
-*/
-  else {
-   
-    //run conveyor and intake to take in power cell
-    RobotContainer::intake->TakeInPowerCell();
-    if (RobotContainer::intake->GetInventory(0) == Intake::StorageState::PRESENT && emptyPositionTriggered == false) {
-      RobotContainer::intake->ConveyorSetSpeed(-.3);
-    }
-
-  }
-  //tag to keep the conveyor from oscillating at five
-  if (RobotContainer::intake->GetInventory(5) == Intake::StorageState::PRESENT) {
-    fiveReached = true;
-  }
-
-  //makes it so the counter will only be reset if the ball is no longer in 5
-  if (RobotContainer::intake->GetInventory(5) == Intake::StorageState::EMPTY) {
-    conveyorBackwardsCounter = 0;
-  }
-
-  // if (RobotContainer::intake->GetPowerCellCount >= 5) {
-  //   RobotContainer::oi->SetControllerRumble(OI::driverController.get(), 1, true);
-  //   RobotContainer::intake->Stop();
-  //   RobotContainer::intake->StopConveyor();
-  // }  
-
-
-
-  // sees if one has been triggered
-  if (RobotContainer::intake->GetInventory(1) == Intake::StorageState::PRESENT) {
-    oneTriggered = true;
-  }
-
-   //sees if the emptyPosition has been triggered
-  if (RobotContainer::intake->GetInventory(emptyPosition) == Intake::StorageState::PRESENT && oneTriggered == true) {
-    emptyPositionTriggered = true;
-  }
-
-  if (emptyPositionTriggered == true && RobotContainer::intake->GetInventory(1) == Intake::StorageState::EMPTY) {
-    badIntake = true;
-    RobotContainer::intake->ConveyorSetSpeed(conveyorBackwardSpeed);
-  }
-
-
-}
+void IntakePowerCell::Execute() {}
 
 // Called once the command ends or is interrupted.
-void IntakePowerCell::End(bool interrupted) {
-  RobotContainer::intake->Stop();
-  RobotContainer::intake->StopConveyor();
-}
+void IntakePowerCell::End(bool interrupted) {}
 
 // Returns true when the command should end.
-bool IntakePowerCell::IsFinished() {
-  //makes sure power cell has advanced to empty position and there aren't any gaps before stopping
-  if (emptyPositionTriggered == true && RobotContainer::intake->GetInventory(1) == Intake::StorageState::PRESENT)
-    return true;
-  else
-    return false; 
-}
+bool IntakePowerCell::IsFinished() { return true; }
