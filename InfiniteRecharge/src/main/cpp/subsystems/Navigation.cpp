b923a52c9075c3f14e1ff2cfd9eb5c4792930988
TrevinM <trevin.macomber@gmail.com>
2020-07-20 19:39:00 -0400

    Added Version

--- InfiniteRecharge/src/main/cpp/subsystems/Navigation.cpp
+++ InfiniteRecharge/src/main/cpp/subsystems/Navigation.cpp
@@ -3,6 +3,7 @@
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
+/* This is version 2020/7/20                                                  */
 /*----------------------------------------------------------------------------*/
 
 #include "subsystems/Navigation.h"
