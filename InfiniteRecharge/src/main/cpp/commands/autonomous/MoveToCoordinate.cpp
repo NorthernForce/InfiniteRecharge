c5a792206c1d7c4c3faceaf44da018be51bdf84c
TrevinM <trevin.macomber@gmail.com>
2020-07-29 20:53:11 -0400

    Removed TurnToAngle

--- InfiniteRecharge/src/main/cpp/commands/autonomous/MoveToCoordinate.cpp
+++ InfiniteRecharge/src/main/cpp/commands/autonomous/MoveToCoordinate.cpp
@@ -5,7 +5,7 @@
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-//Version 14.0
+//Version 15.0
 
 #include "commands/autonomous/MoveToCoordinate.h"
 #include "RobotContainer.h"
@@ -21,9 +21,7 @@ MoveToCoordinate::MoveToCoordinate(int xPos, int yPos, double speed) {
 }
 
 // Called when the command is initially scheduled.
-void MoveToCoordinate::Initialize() {
-  turnToAngle = std::make_shared<TurnToAngle>();
-}
+void MoveToCoordinate::Initialize() {}
 
 // Called repeatedly when this Command is scheduled to run
 void MoveToCoordinate::Execute() {
@@ -44,30 +42,25 @@ void MoveToCoordinate::Execute() {
   distance = sqrt ((xFinal-xCurrent)*(xFinal-xCurrent) + (yFinal-yCurrent)*(yFinal-yCurrent));
   frc::SmartDashboard::PutNumber("distance", distance);
 
-  if (angToFinal < 1) {
-    turnToAngle->End(false);
-  }
   frc::SmartDashboard::PutNumber("turnIsScheduled", turnToAngle->IsScheduled());
-  if (!turnToAngle->IsScheduled()) {
 
-    //Outputs a value that changes how quickly the robot drives
-    distanceSpeed = .1 * (distance > 0) + .1 * (distance >= 1) + .3 * (distance >= 6) + .5 * (distance >= 12);
-    //If robot is more than 10 degrees off -> Turns directly to target. Otherwise it will try to correct and drive.
-    if (abs(angToFinal) > 10) {
-      turnToAngle->SetAngle(angToFinal);
-      turnToAngle->Schedule();
-    }
-    else {
-      //(angToFinal/k); k scales correction while driving. k -> 0; correction increases.
-      distanceSpeed = distanceSpeed - (abs(angToFinal/15) * distanceSpeed);
-      leftPower = (distanceSpeed - (angToFinal/15)) * baseSpeed;
-      rightPower = (distanceSpeed + (angToFinal/15)) * baseSpeed;
-      RobotContainer::drivetrain->DriveUsingSpeeds(leftPower,rightPower);
-      frc::SmartDashboard::PutNumber("leftPower",leftPower);
-      frc::SmartDashboard::PutNumber("rightPower",rightPower);
+   //Outputs a value that changes how quickly the robot drives
+   distanceSpeed = .1 * (distance > 0) + .1 * (distance >= 1) + .3 * (distance >= 6) + .5 * (distance >= 12);
+   //If robot is more than 10 degrees off -> Turns directly to target. Otherwise it will try to correct and drive.
+   if (abs(angToFinal) > 10) {
+     turnToAngle->SetAngle(angToFinal);
+     turnToAngle->Schedule();
+   }
+   else {
+     //(angToFinal/k); k scales correction while driving. k -> 0; correction increases.
+     distanceSpeed = distanceSpeed - (abs(angToFinal/15) * distanceSpeed);
+     leftPower = (distanceSpeed - (angToFinal/15)) * baseSpeed;
+     rightPower = (distanceSpeed + (angToFinal/15)) * baseSpeed;
+     RobotContainer::drivetrain->DriveUsingSpeeds(leftPower,rightPower);
+     frc::SmartDashboard::PutNumber("leftPower",leftPower);
+     frc::SmartDashboard::PutNumber("rightPower",rightPower);
     }
   }  
-}
 
 // Called once the command ends or is interrupted.
 void MoveToCoordinate::End(bool interrupted) {
