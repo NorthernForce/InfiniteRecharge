3f2e1a119cd0de4238c7aaf83a48f627cee67a2b
ADever <aiden.dever@gorhamschools.org>
2021-04-07 17:52:20 -0400

    shooter

--- InfiniteRecharge/src/main/cpp/commands/ManualShooter.cpp
+++ InfiniteRecharge/src/main/cpp/commands/ManualShooter.cpp
@@ -21,7 +21,7 @@ void ManualShooter::Execute() {
   if (rtTriggerAxis > 0.5) {
     RobotContainer::shooter->SetRawSpeed(-0.9);
 
-    if (RobotContainer::shooter->GetError() < 100)
+    if (RobotContainer::shooter->GetError() < -100)
         RobotContainer::intake->ConveyorSetSpeed(conveyorShooterSpeed);
     else if (RobotContainer::intake->GetInventory(4) == Intake::StorageState::EMPTY)
         RobotContainer::intake->RunConveyor();
